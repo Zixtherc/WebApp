8698fdcbceff7c447892a70cef8de16d65325e74
Zixther <duckandfiretto@gmail.com>
2025-04-09 17:49:45 +0300

    Fixed an error

--- Project/urls.py
+++ Project/urls.py
@@ -1,3 +1,7 @@
 from home_app import app, render_home
+from .settings import project
+
 
 app.home.add_url_rule(rule= "/", view_func = render_home)
+
+project.register_blueprint(blueprint = app.home)
